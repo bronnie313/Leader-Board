66d9ef7cd75780d12180335c69e43af2dd89a929
Bisaso Ronald <bronnie313@gmail.com>
2023-05-18 12:42:45 +0300

    add function to fetch data from api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import addData from './modules/addData';
+
+const getData = async () => {
+    const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/4hUxjWxbhx8eZYEYlSSm/scores/');
+    const data = await res.json();
+    return data.result;
+}
